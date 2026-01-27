0355a59236e04ef7ec0c548cd06b384ad7ef80c4
martinbellot <martin.bellot.off@gmail.com>
2026-01-27 14:42:25 +0100

    (+) fix bugs

--- api/contact/admin.py
+++ api/contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Contact 
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email', 'message')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
